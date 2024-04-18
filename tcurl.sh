5cb6263394ddea91db0205f68cdee8903333a372
Tomi Raittinen <tomi.raittinen@cloud2.fi>
2024-04-18 14:19:28 +0300

    Interval as variable, connect time added

--- tcurl.sh
+++ tcurl.sh
@@ -6,6 +6,9 @@ curls_ok=0
 curls_nok=0
 start_time=`date +%s%3N`
 time_cumulative=0
+interval=1
+time_min=999
+time_max=0
 
 if [ -z $url ]; then
     echo "Usage: `basename $0` [URL]"
@@ -23,7 +26,7 @@ control_c()
 
   if [ $curls_ok -gt 0 ]; then
         time_avg=$(echo "scale=2; $time_cumulative / $curls_ok" | bc)
-        echo -en "Time average for code 200 gets $time_avg s\n"
+        echo -en "Time average for code 200 gets $time_avg s, min $time_min s, max $time_max s\n"
   fi
 
   echo -en "\n\n"
@@ -35,21 +38,24 @@ control_c()
 trap control_c SIGINT
 
 while :; do
-    response=$(curl -L --write-out "%{url_effective};%{http_code};%{time_total}\n" --silent --max-time 1 --output /dev/null "$url")
+    response=$(curl -L --write-out "%{url_effective};%{http_code};%{time_total};%{time_connect}\n" --silent --max-time $interval --output /dev/null "$url")
 	url_effective="$( cut -d ';' -f 1 <<< "$response" )"
 	http_code="$( cut -d ';' -f 2 <<< "$response" )"
 	time_total="$( cut -d ';' -f 3 <<< "$response" )"
+	time_connect="$( cut -d ';' -f 4 <<< "$response" )"
 
     if [ $http_code -ne 200 ]; then
-        echo -e "`date +'%Y/%m/%d %H:%M:%S %Z'` - $url_effective - $http_code - $time_total - \033[0;31mNOT OK\033[0m"
+        echo -e "`date +'%Y/%m/%d %H:%M:%S %Z'` - $url_effective - $http_code - Time $time_total ($time_connect connect) - \033[0;31mNOT OK\033[0m"
         let curls_sent++
         let curls_nok++
     else
-        echo -e "`date +'%Y/%m/%d %H:%M:%S %Z'` - $url_effective - $http_code - $time_total - \033[0;32mok\033[0m"
-		time_cumulative=$(echo "$time_cumulative + $time_total" | bc)
-		let curls_sent++
+        echo -e "`date +'%Y/%m/%d %H:%M:%S %Z'` - $url_effective - $http_code - Time $time_total ($time_connect connect) - \033[0;32mok\033[0m"
+	time_cumulative=$(echo "$time_cumulative + $time_total" | bc)
+        if [ $(echo "$time_total > $time_max" | bc -l) -eq 1 ]; then time_max=$time_total; fi
+        if [ $(echo "$time_total < $time_min" | bc -l) -eq 1 ]; then time_min=$time_total; fi
+	let curls_sent++
         let curls_ok++
     fi
 
-    sleep 1
+    sleep $interval
 done
